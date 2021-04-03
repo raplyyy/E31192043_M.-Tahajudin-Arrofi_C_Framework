a09fb7ce8b3a2f067e3d0dd723b04813a53b0309
raplyyy <mtarrofi4@gmail.com>
2021-04-04 00:04:04 +0700

    Helper

--- Minggu9/Latihan/ci3/application/config/routes.php
+++ Minggu9/Latihan/ci3/application/config/routes.php
@@ -53,5 +53,5 @@ $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
-$route['prodi'] = 'Mahasiswa/prodi';
-$route['angkatan'] = 'Mahasiswa/angkatan';
+$route['profil'] = 'welcome/profil';
+$route['contact'] = 'welcome/contact';
--- Minggu9/Latihan/ci3/application/views/contact.php
+++ Minggu9/Latihan/ci3/application/views/contact.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>E31192043 - M. Tahajudin Arrofi</title>
+    <title>Document</title>
 </head>
 <body>
-    <p>Nama : <?= $nama; ?></p>
-    <p>Prodi : <?= $prodi; ?></p>
+    <h2>Halaman Contact</h2>
+    <p>selamat datang ini adalah halaman contact</p>
 </body>
 </html>
